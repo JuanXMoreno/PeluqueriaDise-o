6d783089bb0fe043e52524d5dbbe963e3645cf52
Gabriel Farias <lucianofarias271@gmail.com>
2022-10-06 15:25:09 -0300

    No se enviaron estos datos

---
+++ VisorDeBajas.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ProyectoPeluquería
+{
+    partial class VisorDeBajas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisorDeBajas));
+            this.BtnBusqueda = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TexboxBuscador = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnBusqueda
+            // 
+            this.BtnBusqueda.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnBusqueda.BackgroundImage")));
+            this.BtnBusqueda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnBusqueda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBusqueda.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold);
+            this.BtnBusqueda.ForeColor = System.Drawing.Color.White;
+            this.BtnBusqueda.Location = new System.Drawing.Point(582, 28);
+            this.BtnBusqueda.Name = "BtnBusqueda";
+            this.BtnBusqueda.Size = new System.Drawing.Size(82, 30);
+            this.BtnBusqueda.TabIndex = 28;
+            this.BtnBusqueda.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.BtnBusqueda.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BtnBusqueda.UseVisualStyleBackColor = true;
+            this.BtnBusqueda.Click += new System.EventHandler(this.BtnBusqueda_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Buscar Stock";
+            // 
+            // TexboxBuscador
+            // 
+            this.TexboxBuscador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TexboxBuscador.Location = new System.Drawing.Point(12, 28);
+            this.TexboxBuscador.Name = "TexboxBuscador";
+            this.TexboxBuscador.Size = new System.Drawing.Size(564, 30);
+            this.TexboxBuscador.TabIndex = 29;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 301);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Actualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.F5);
+            // 
+            // VisorDeBajas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(676, 406);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnBusqueda);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TexboxBuscador);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "VisorDeBajas";
+            this.Text = "VisorDeBajas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBusqueda;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TexboxBuscador;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
