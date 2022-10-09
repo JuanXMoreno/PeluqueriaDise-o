db0027750161af825c5f8b24040c992c2d9ba348
Gabriel Farias <lucianofarias271@gmail.com>
2022-10-09 02:02:51 -0300

    Cree el Form Config y lo diseñe un poco Faltan bastantes cosas aun pero va queriendo

---
+++ FormConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPeluquería
+{
+    public partial class FormConfig : Form
+    {
+        public FormConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSQL_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtServer.Text);
+        }
+
+        private void Cerrar_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            FormAdmin F1 = Owner as FormAdmin;
+            F1.Visible = true;
+        }
+    }
+}
