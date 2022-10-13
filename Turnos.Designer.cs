fbb515d15a11cba2c054e373d25db32584fd7706
Jazmin Mendieta <Jazmin Mendieta@DESKTOP-SK840FQ>
2022-10-13 18:52:59 -0300

    Cambios en la interfaz Cambios en las propiedades de botones La pantalla de inicio mejoro

---
+++ Turnos.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ProyectoPeluquería
+{
+    partial class Turnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.cmbPeluquero = new System.Windows.Forms.ComboBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.txtHora = new System.Windows.Forms.DateTimePicker();
+            this.txtDia = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dia:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hora:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cliente:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Peluquero \r\nencargado:";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(73, 71);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(183, 20);
+            this.txtCliente.TabIndex = 3;
+            this.txtCliente.TextChanged += new System.EventHandler(this.txtCliente_TextChanged);
+            // 
+            // cmbPeluquero
+            // 
+            this.cmbPeluquero.FormattingEnabled = true;
+            this.cmbPeluquero.ItemHeight = 13;
+            this.cmbPeluquero.Location = new System.Drawing.Point(73, 100);
+            this.cmbPeluquero.Name = "cmbPeluquero";
+            this.cmbPeluquero.Size = new System.Drawing.Size(183, 21);
+            this.cmbPeluquero.TabIndex = 7;
+            this.cmbPeluquero.SelectedIndexChanged += new System.EventHandler(this.cmbPeluquero_SelectedIndexChanged);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Enabled = false;
+            this.btnInsertar.Location = new System.Drawing.Point(24, 161);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(83, 41);
+            this.btnInsertar.TabIndex = 5;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(113, 161);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(83, 40);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(202, 161);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(83, 40);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(291, 12);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.Size = new System.Drawing.Size(410, 190);
+            this.dgvDatos.TabIndex = 11;
+            this.dgvDatos.TabStop = false;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnVolver.Location = new System.Drawing.Point(608, 208);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(93, 40);
+            this.btnVolver.TabIndex = 12;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // txtHora
+            // 
+            this.txtHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.txtHora.Location = new System.Drawing.Point(73, 45);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(183, 20);
+            this.txtHora.TabIndex = 2;
+            this.txtHora.ValueChanged += new System.EventHandler(this.txtHora_ValueChanged);
+            // 
+            // txtDia
+            // 
+            this.txtDia.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtDia.Location = new System.Drawing.Point(73, 19);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(183, 20);
+            this.txtDia.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDia);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtHora);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCliente);
+            this.groupBox1.Controls.Add(this.cmbPeluquero);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(273, 143);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Turnos";
+            // 
+            // Turnos
+            // 
+            this.AcceptButton = this.btnInsertar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnVolver;
+            this.ClientSize = new System.Drawing.Size(713, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Turnos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Turnos";
+            this.Load += new System.EventHandler(this.Turnos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.ComboBox cmbPeluquero;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.DateTimePicker txtHora;
+        private System.Windows.Forms.DateTimePicker txtDia;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Turnos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ProyectoPeluquería
+    
+{
+    
+
+
+
+    public partial class Turnos : Form
+    {
+        private int n=0;
+
+
+
+        public Turnos()
+            
+
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Turnos_Load(object sender, EventArgs e)
+        {  
+
+            dgvDatos.Columns.Add("dia", "dia");
+            dgvDatos.Columns.Add("hora", "hora");
+            dgvDatos.Columns.Add("cliente", "cliente");
+            dgvDatos.Columns.Add("peluquero", "peluquero");
+
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            dgvDatos.Rows.Add(txtDia.Text, txtHora.Text, txtCliente.Text);
+            txtDia.Text = "";
+            txtHora.Text =String.Empty;
+            txtCliente.Text = String.Empty;
+            cmbPeluquero.Text=String.Empty; 
+
+
+
+        }
+
+        
+
+         private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            n = e.RowIndex;
+          
+          }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            dgvDatos.Rows.RemoveAt(n);
+        }
+
+        private void txtDia_TextChanged(object sender, EventArgs e)
+        {
+            HabilitarBoton();
+        }
+
+        void HabilitarBoton()
+        {
+            if (txtDia.Text !="" && txtHora.Text != "" && txtCliente.Text !="")
+                btnInsertar.Enabled = true;
+            else btnInsertar.Enabled = false;
+        }
+
+        private void txtCliente_TextChanged(object sender, EventArgs e)
+        {
+            HabilitarBoton();
+        }
+
+        private void txtHora_ValueChanged(object sender, EventArgs e)
+        {
+            HabilitarBoton();
+        }
+
+        private void cmbPeluquero_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HabilitarBoton();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
