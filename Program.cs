92919beb91e3d8e3bc97ce2d450be11e05a4b72f
Juan Cruz Moreno <93409756+JuanXMoreno@users.noreply.github.com>
2022-10-13 19:16:21 -0300

    prueba

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace ProyectoPeluquería
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form2());
+            //prueba
         }
     }
 }
