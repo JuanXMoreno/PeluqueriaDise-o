fd947d397bded49b7239f179bf548012ea6ccf1e
Gabriel Farias <lucianofarias271@gmail.com>
2022-10-27 00:01:19 -0300

    Modifique el inicio de mi form y el codigo de acceso a la base de datos.

--- Login.cs
+++ Login.cs
@@ -14,7 +14,7 @@ namespace ProyectoPeluquería
             txtContraseña.PasswordChar = '*';
         }
 
-        SqlConnection conexion = new SqlConnection("server=DESKTOP-COF6H2T;database=Peluqueria; integrated security=true");
+        SqlConnection conexion = new SqlConnection(@"server=DESKTOP-GGALNHK\SQLEXPRESS01;database=Peluqueria; integrated security=true");
 
         //boton para ingresar al administrador con el usuario y contraseña guardadas en la base de datos
         private void button1_Click(object sender, EventArgs e)
