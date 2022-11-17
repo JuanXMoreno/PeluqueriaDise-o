cfa2fa1ebd2abc5799e4e75f32dcc871be0769ed
Juan Cruz Moreno <93409756+JuanXMoreno@users.noreply.github.com>
2022-11-16 22:49:21 -0300

    todo x2

--- DataBase.cs
+++ DataBase.cs
@@ -7,7 +7,9 @@ namespace ProyectoPeluquería
 {
     class DataBase
     {
-        public static string link = @"SERVER=(localdb)\Home;DATABASE=Peluqueria;integrated security=true"; //Agrege esto para no tener que cambiar manualmente la clave en cada metodo
+        //DESKTOP-COF6H2T Juan
+        //(localdb)\Home lucho
+        public static string link = @"SERVER=DESKTOP-COF6H2T;DATABASE=Peluqueria;integrated security=true"; //Agrege esto para no tener que cambiar manualmente la clave en cada metodo
         SqlConnection Conectarse = null;
         SqlCommand cmd = null;
         SqlTransaction Tran = null;
